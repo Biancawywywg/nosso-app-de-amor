3b4bae8e7c793c278d3f9afb21e19f18c2298d07
Biancawywywg <ericabianca.2101.eb11@gmail.com>
2025-07-15 00:01:02 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+{
+  "name": "App do Amor - E 💖 F",
+  "short_name": "E 💖 F",
+  "description": "Nosso cantinho especial para viver e registrar nosso amor!",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#1a1a1a",
+  "theme_color": "#ff7aa2",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
